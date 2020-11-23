31cfec1f7becec3c6b2164bedd58eba86167a675
nnnamani <6526114+nnnamani@users.noreply.github.com>
2020-11-23 11:29:28 +0900

    Add test

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub fn greet_message() -> String {
     String::from("Hello, Rust!")
 }
 
+pub fn add(left: u32, right: u32) -> u32 {
+    left + right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,5 +13,19 @@ mod tests {
     #[test]
     fn test_greet_message_is_hello_rust() {
         assert_eq!("Hello, Rust!", greet_message());
+    }
+
+    #[test]
+    fn test_add_should_add_left_and_right() {
+        let left = 1;
+        let right = 2;
+        assert_eq!(left + right, add(left, right));
+    }
+
+    #[test]
+    fn test_add_should_not_muoltiply_left_and_right() {
+        let left = 1;
+        let right = 2;
+        assert_ne!(left * right, add(left, right));
     }
 }
